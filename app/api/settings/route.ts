53d3ccabeedc3f200c9b66537fb397499d1779fd
ARedaUni <w1821480@my.westminster.ac.uk>
2025-01-02 14:40:20 +0000

    fixed a ts error in settings

--- app/api/settings/route.ts
+++ app/api/settings/route.ts
@@ -7,14 +7,12 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
-  // @ts-ignore
-  if (!global.prisma) {
-    // @ts-ignore
-    global.prisma = new PrismaClient();
-  }
-  // @ts-ignore
+  global.prisma = global.prisma || new PrismaClient();
   prisma = global.prisma;
 }
+
+prisma = global.prisma;
+
 
 export async function POST(req: NextRequest) {
   try {
